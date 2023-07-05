831cb596540e63197b9beef38a21ea627f5746f6
eefendic1 <115942770+eefendic1@users.noreply.github.com>
2023-07-05 10:12:56 +0200

    Small update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from scipy.signal import medfilt, butter, lfilter, iirfilter
 import matplotlib.pyplot as plt
 
 
-
 # Median filter
 def median(signal):
     array = np.array(signal)
@@ -12,7 +11,6 @@ def median(signal):
     return med_filtered
 
 
-# Butterworth filter
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
@@ -21,6 +19,7 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     return b, a
 
 
+# Butterworth filter
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
@@ -56,12 +55,12 @@ def lowpass_filter(signal, highcut, fs, order=5):
     return filtered_signal
 
 
-# Band-stop filter
-def bandstop_filter(signal, fs, lowcut, highcut):
+# Band-pass filter
+def bandpass_filter(signal, fs, lowcut, highcut):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
     high = highcut / nyquist
-    b, a = butter(4, [low, high], btype='bandstop')
+    b, a = butter(4, [low, high], btype='bandpass')
     filtered_signal = lfilter(b, a, signal)
     return filtered_signal
 
@@ -74,15 +73,27 @@ def load_eeg_signal(filename):
     return eeg_signal
 
 
+"""""
+In case of using dataset with defined electrodes in each column
+
+def load_eeg_signal(filename):
+    data = pd.read_csv(filename)
+    print(data.columns)  # Print the column names to verify their correctness
+    eeg_signal = data.values[:, :-1]
+    return eeg_signal
+    
+"""""
+
+
 # Generate image
 def generate_image(signal, filtered_signal):
     plt.figure(figsize=(12, 6))
     plt.subplot(2, 1, 1)
     plt.plot(signal)
-    plt.title('Originalni EEG signal')
+    plt.title('Original EEG signal')
     plt.subplot(2, 1, 2)
     plt.plot(filtered_signal)
-    plt.title('Filtrirani EEG signal')
+    plt.title('Filtered EEG signal')
     plt.tight_layout()
     plt.savefig('eeg_signal_comparison.png')  # Save the image as 'eeg_signal_comparison.png'
     plt.show()
@@ -90,41 +101,37 @@ def generate_image(signal, filtered_signal):
 
 # Separation of frequency bands
 def separate_frequency_bands(signal, fs):
-    # Apply Fourier transform to convert signal to frequency domain
     frequency_domain = np.fft.fft(signal)
 
-    # Frequency range
     n = len(signal)
     frequency = np.fft.fftfreq(n, d=1.0 / fs)
 
-    # Define frequency band ranges (in Hz)
     delta_band = (0.5, 4)
     theta_band = (4, 8)
     alpha_band = (8, 12)
     beta_band = (12, 30)
     gamma_band = (30, 100)
 
-    # Find indices corresponding to each frequency band
     delta_indices = np.where(np.logical_and(frequency >= delta_band[0], frequency < delta_band[1]))[0]
     theta_indices = np.where(np.logical_and(frequency >= theta_band[0], frequency < theta_band[1]))[0]
     alpha_indices = np.where(np.logical_and(frequency >= alpha_band[0], frequency < alpha_band[1]))[0]
     beta_indices = np.where(np.logical_and(frequency >= beta_band[0], frequency < beta_band[1]))[0]
     gamma_indices = np.where(np.logical_and(frequency >= gamma_band[0], frequency < gamma_band[1]))[0]
 
-    # Extract amplitude spectra for each frequency band
     delta_spectrum = np.abs(frequency_domain[delta_indices])
     theta_spectrum = np.abs(frequency_domain[theta_indices])
     alpha_spectrum = np.abs(frequency_domain[alpha_indices])
     beta_spectrum = np.abs(frequency_domain[beta_indices])
     gamma_spectrum = np.abs(frequency_domain[gamma_indices])
 
-    return (delta_spectrum, theta_spectrum, alpha_spectrum, beta_spectrum, gamma_spectrum)
+    return delta_spectrum, theta_spectrum, alpha_spectrum, beta_spectrum, gamma_spectrum
 
 
 # Generate plots for frequency bands
 def generate_combined_plots(signal, filtered_signal, fs):
     delta_spectrum, theta_spectrum, alpha_spectrum, beta_spectrum, gamma_spectrum = separate_frequency_bands(signal, fs)
-    delta_filtered_spectrum, theta_filtered_spectrum, alpha_filtered_spectrum, beta_filtered_spectrum, gamma_filtered_spectrum = separate_frequency_bands(filtered_signal, fs)
+    delta_filtered_spectrum, theta_filtered_spectrum, alpha_filtered_spectrum, beta_filtered_spectrum, gamma_filtered_spectrum = separate_frequency_bands(
+        filtered_signal, fs)
 
     fig, axs = plt.subplots(5, 2, figsize=(12, 9))
     plt.subplots_adjust(bottom=0.2)
@@ -165,7 +172,7 @@ def generate_combined_plots(signal, filtered_signal, fs):
 
 def main():
     # Load EEG signal from file
-    eeg_signal = load_eeg_signal('C:/Users/User-PC/Desktop/dps/raw1.csv')
+    eeg_signal = load_eeg_signal('C:/Users/User-PC/Desktop/dps/raw4.csv')
 
     # User input for filter selection
     filter_choice = input(
@@ -185,25 +192,26 @@ def main():
         fs = 512  # Sampling frequency (Hz)
         freq = 60.0  # Notch frequency to remove (e.g., power line interference)
         ripple = 5.0  # Passband ripple (dB)
-        order = 8  # Filter order
-        filter_type = 'cheby1'  # Filter type (e.g., Butterworth)
+        order = 5  # Filter order
+        filter_type = 'cheby1'  # Filter type
         filtered_signal = notch_filter(eeg_signal, fs, freq, ripple, order, filter_type)
     elif filter_choice == '4':
         # Apply High-pass filter
-        lowcut = 0.1  # Lower cutoff frequency
+        lowcut = 0.5  # Lower cutoff frequency
         fs = 512  # Sampling frequency (Hz)
         filtered_signal = highpass_filter(eeg_signal, lowcut, fs, order=5)
     elif filter_choice == '5':
         # Apply Low-pass filter
-        highcut = 50.0  # Upper cutoff frequency
+        highcut = 30.0  # Upper cutoff frequency
         fs = 512  # Sampling frequency (Hz)
         filtered_signal = lowpass_filter(eeg_signal, highcut, fs, order=5)
     elif filter_choice == '6':
-        # Apply Band-stop filter
-        lowcut = 45.0  # Lower cutoff frequency
-        highcut = 55.0  # Upper cutoff frequency
+        # Apply Bandpass filter
+        lowcut = 10  # Lower cutoff frequency
+        highcut = 20  # Upper cutoff frequency
         fs = 512  # Sampling frequency (Hz)
-        filtered_signal = bandstop_filter(eeg_signal, fs, lowcut, highcut)
+        filtered_signal = bandpass_filter(eeg_signal, fs, lowcut, highcut)
+
     else:
         print("Invalid filter choice.")
         return
@@ -212,10 +220,8 @@ def main():
     generate_image(eeg_signal, filtered_signal)
     fs = 512  # Define the default sampling frequency (Hz)
     # Generate frequency band plots
-    generate_combined_plots(eeg_signal, filtered_signal , fs);
-
+    generate_combined_plots(eeg_signal, filtered_signal, fs)
 
 
 if __name__ == "__main__":
     main()
-
